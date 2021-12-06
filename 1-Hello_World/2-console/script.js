66bd5c3492e2b21a83cd8318989b66c472921f55
Daniel Mrkoci <daniel@wiklunds.nu>
2021-12-06 10:00:32 +0100

    Lektion 1

---
+++ 1-Hello_World/2-console/script.js
@@ -0,0 +1,28 @@
+// en kommentar på en rad (ctrl + *)
+/*
+  en kommentar kan skrivas såhär
+  on det är flera rader (shift + alt + a)
+*/
+
+// Skriver ut någonting i konsolen
+console.log('Här skriver jag ut någonting i konsolen');
+console.warn('Detta är en varning');
+console.error('Detta är ett felmeddelande');
+
+//console.lg('Hej')
+
+// Skriver ut på själva hemsidan
+document.body.innerText = 'Detta är en text'
+
+
+// Högnivå språk (mellan användaren och systemet)
+// ---------------------------------------------
+// (Javascript)
+// Python
+// C# / Java
+
+// Lågnivå språk (mellan systemet och hårdvara)
+// -------------------------------------------
+// C++
+// C
+// Mikrokod

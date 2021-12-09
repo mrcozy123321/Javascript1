6925111c95e4ae8427973e3691b67ddc019fa1c1
Daniel Mrkoci <daniel@wiklunds.nu>
2021-12-09 19:27:22 +0100

    lektion 2

---
+++ Lektion-2/1-repetition/script.js
@@ -0,0 +1,34 @@
+// CONSOLE
+
+//Felsökning om något går fel
+// let app;
+
+// console.log('Vanligt meddelande');
+// console.warn('Varnings meddelande');
+// console.error('Felmeddelande');
+
+//DOM
+// document.body.innerText = '<h1>Hejsan</h1>';
+// document.body.innerHTML = '<h1>Hejsan</h1>';
+// document.body.createElement('div').innerText = 'hej'; // ger ett felmeddelande i consolen
+// document.body.appendChild(document.createElement('div'));
+
+// const results = document.querySelector('#results')
+// // const input = document.querySelector('.input-class');
+// const input = document.getElementsByName('input-name')[0];
+
+// let res = document.querySelector('#input-id').value;
+
+// res = input.classList
+
+// results.innerText = res;
+// console.log(input);
+// console.log(res);
+
+
+// VARIABLES
+
+// name = 'Daniel';          // deklarerar något glovalt. GÖR INTE SÅHÄR!
+// var name = 'Daniel';      // deklarerar någonting globalt. UNDVIK DET HÄR!
+// let name = 'Daniel';      // deklarerar någonting lokalt i ett scope. GÖR DET HÄR!
+const name = 'Daniel';       // deklarerar någonting skrivskyddat lokalt. GÖR DET HÄR!

8bacd3d21ab05810e02e024c2b1d3ee2498bc36d
Daniel Mrkoci <daniel@wiklunds.nu>
2021-12-13 15:53:30 +0100

    lektion 2

--- Lektion-2/2-arrays/script.js
+++ Lektion-2/2-arrays/script.js
@@ -109,9 +109,9 @@ const obj3 = {
 
 
 const newNames = ['Aa', 'Bb', '1a', '2a', 'aa', 'bb'];
-const numbers = [5, 6, 7, 8, 2, 1, 9, 10, 11 ,12, 30, 40];
+// const numbers = [5, 6, 7, 8, 2, 1, 9, 10, 11 ,12, 30, 40];
 
-console.log(names);
+// console.log(names);
 
 // names.sort();   // Sortera i bokstavsordning
 // console.log(names);
@@ -122,9 +122,40 @@ console.log(names);
 // console.log(names);
 
 newNames.sort();
-console.log(newNames)
+// console.log(newNames)
 
-numbers.sort((a, b) => {
-  return a - b;
-});
-console.log(numbers);
+// numbers.sort((a, b) => {
+//   return a - b;
+// });
+
+// console.log(numbers);
+
+
+// High Order Array Methods
+// en high order method tar antingen emot eller returnerar en funktion
+
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// const sum = numbers.reduce(function(acc, current) {
+//   console.log('acc: ' + acc, 'current: ' + current);
+//   return acc + current
+// });
+
+// const filteredArray = numbers.filter(num => {
+//   return num < 6;
+// })
+
+// console.log(filteredArray);
+// console.log(sum);
+
+// const mappedArray = numbers.map((num, index) => {
+//   return `index: ${index} - number: ${num}`
+// })
+// console.log(mappedArray);
+
+// const test = []
+// numbers.forEach(num => {
+//   num++;
+// })
+
+// console.log(numbers)

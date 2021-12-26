b5aa2c63db0b1e4f1b63d2509847cb9df879babd
Daniel Mrkoci <daniel@wiklunds.nu>
2021-12-26 15:46:36 +0100

    lektion 4

--- Lektion-4/1-repetition/script.js
+++ Lektion-4/1-repetition/script.js
@@ -100,3 +100,138 @@ const filteredUsers = newUsers.filter(user => {
 })
 // console.log(filteredUsers);
 
+
+//    WHILE / DO WHILE
+
+// let index = 0;
+
+// while(index < 20) {
+//   console.log('.')
+//   index++;
+// }
+
+// do {
+//   console.log('dowhile')
+//   index++;
+// }while(index < 0);
+
+
+
+//  FUNCTIONS
+
+// function displayFoodItems() {
+//   for(let item of items) {
+//     foodItemsUL.innerHTML += `<li>${item}</li>`
+//   }
+// }
+
+
+// displayFoodItems()
+
+// const printToDOM = (array) => {
+//   array.forEach(item => {
+//   document.body.innerHTML += `<div>${item}</div>`
+//   })
+// }
+
+// printToDOM(['hej', 'test'])
+
+
+// METHODS
+
+let lastName = 'Andersson';
+
+const person1 = {
+  firstName: 'Daniel',
+  lastName: 'Mrkoci',
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+const person2 = {
+  firstName: 'Elias',
+  lastName: 'Broberger',
+
+  fullName() {
+    return `${this.firstName} ${this.lastName}`
+  }
+}
+
+// console.log(person1.fullName());
+// console.log(person2.fullName());
+
+
+
+// Bad practice
+const calculateTemperature = (temp) => {
+  // calculate celcius
+  // calculate farenheit
+  // calculate kelvin
+}
+
+// Good practice
+const calculateCelcium = (temp) => {}
+const calculateFarenheit = (temp) => {}
+const calculateKelvin = (temp) => {}
+
+
+// Bad
+function register() {
+  // validate inputs
+  // register user
+  // login user
+}
+
+// Good
+function validateInputs() {}
+function registerUser() {}
+function loginUser() {}
+
+
+const checkUserEmailExists = (email) => {
+  // kollar på databasen om mailen är ledig
+  return true;
+}
+
+function registerUser() {
+  if(checkUserEmailExists(email)) {
+    // throw new Error()
+    return false;
+  } else {
+    // register user
+    return true;
+  }
+}
+
+
+function onSignUp() {
+  if(validateInputs()) {
+    if(registerUser()) {
+      if(loginUser()) {
+
+      }
+    }
+  }
+
+  if(!validateInputs())
+    return;
+
+  if(!registerUser())
+    return;
+}
+
+
+
+// Callback functions
+
+const powerOf = (nr1, nr2, cb) => {
+  let sum = nr1 ** nr2;
+
+  cb(sum)
+}
+
+powerOf(2,3, (result) => {
+  console.log(result)
+})
